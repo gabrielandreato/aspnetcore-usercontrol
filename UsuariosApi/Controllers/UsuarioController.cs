9ae9b4820f8039d622ad7e2c7814cdd5a8274d9e
Daniel Artine Mastub <daniel.mastub@stoneage.com.br>
2023-02-21 22:19:18 -0300

    Aula-2

---
+++ UsuariosApi/Controllers/UsuarioController.cs
@@ -0,0 +1,38 @@
+﻿
+using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using UsuariosApi.Data.Dtos;
+using UsuariosApi.Models;
+
+namespace UsuariosApi.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        private IMapper _mapper;
+        private UserManager<Usuario> _userManager;
+
+        public UsuarioController(IMapper mapper, UserManager<Usuario> userManager)
+        {
+            _mapper = mapper;
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CadastraUsuario
+            (CreateUsuarioDto dto)
+        {
+            Usuario usuario = _mapper.Map<Usuario>(dto);
+
+            IdentityResult resultado = await _userManager.CreateAsync(usuario, dto.Password);
+
+            if (resultado.Succeeded) return Ok("Usuário cadastrado!");
+
+            throw new ApplicationException("Falha ao cadastrar usuário!");
+
+
+        }
+    }
+}
---
+++ UsuariosApi/Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using UsuariosApi.Data;
+using UsuariosApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+var connString = builder.Configuration.GetConnectionString("UsuarioConnection");
+
+builder.Services.AddDbContext<UsuarioDbContext>
+    (opts =>
+    {
+        opts.UseMySql(connString, ServerVersion.AutoDetect(connString));
+    });
+
+builder.Services
+    .AddIdentity<Usuario, IdentityRole>()
+    .AddEntityFrameworkStores<UsuarioDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
