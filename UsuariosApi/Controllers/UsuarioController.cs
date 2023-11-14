6f420e4900deb4e9eaf01bcb868ef6b6221311ce
gabrielandreato <97205888+gabrielandreato@users.noreply.github.com>
2023-11-13 22:10:49 -0300

    Implementing Controller, implementing gitignore

--- UsuariosApi/Controllers/UsuarioController.cs
+++ UsuariosApi/Controllers/UsuarioController.cs
@@ -1,18 +1,33 @@
 ﻿using System;
+using AutoMapper;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using UsuariosApi.Data.Dtos;
+using UsuariosApi.Models;
 
-namespace UsuariosApi.Controllers
+namespace UsuariosApi.Controllers;
+
+[ApiController]
+[Route("[Controller]")]
+public class UsuarioController : ControllerBase
 {
-    [ApiController]
-    [Route("[Controller]")]
-    public class UsuarioController : ControllerBase
+    private readonly IMapper _mapper;
+    private readonly UserManager<Usuario> _userManager;
+
+    public UsuarioController(IMapper mapper, UserManager<Usuario> userManager)
     {
-        
-        [HttpPost]
-        public Task<IActionResult> CadastraUsuario(CreateUsuarioDto dto)
-        {
-            throw new NotImplementedException();
-        }
+        _mapper = mapper;
+        _userManager = userManager;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CadastraUsuario(CreateUsuarioDto createUsuarioDto)
+    {
+        var usuario = _mapper.Map<CreateUsuarioDto, Usuario>(createUsuarioDto);
+
+        var resultado = await _userManager.CreateAsync(usuario);
+
+        if (resultado.Succeeded) return Ok("Usuario cadastrado.");
+        throw new ApplicationException("Falha ao cadastrar usuário!");
     }
 }
